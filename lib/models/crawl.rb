b08db2e16a8c18d0e60110b49a71429eaea6bb1a
Graham Ormiston <grahamormiston@gmail.com>
2016-11-03 13:03:32 +0000

    edge case for urls starting with //

--- lib/models/crawl.rb
+++ lib/models/crawl.rb
@@ -17,38 +17,56 @@ class Crawl
 
   def crawl
     url = Addressable::URI.parse(@url)
+    p url
     loop_through_links(url)
-    # check status of each link to ensure they are available?
-    # store the response.body along with the link in a hash like link:body
-    # iterate through the hash to find all the static assets in the
+    # print page or send to page
   end
 
   def loop_through_links(url)
     while @site_info[:links_found] > @site_info[:links_crawled] do
       #  loops through available, uncrawled urls in the @processed array
       #  something like if @processed.any? x[0] == (link) && [1] == false
-        body = get_body(url)
-        process_links(body)
+      @processed.each do |link_array|
+        if link_array[1] == false
+          p "crawling: " + link_array[0]
+          body = get_body(url)
+          process_links(body)
+          link_array[1] = true
+          @site_info[:links_crawled] += 1
+        # elsif link_array[2] == false
+        #   process_assets(body)
+        #   link_array[2] = true
+        end
+      end
+
+      p @site_info[:links_found]
+      p @site_info[:links_crawled]
       #  then check if assets == true
       # process_assets(body)
       # else move on i++
     end
+    p "complete"
   end
 
   def process_links(body)
-    # access array for the url
     document = parse_html(body)
     i = 0
     count = document.css('a').count
     while i < count do
       link = document.css('a')[i]['href']
-      if internal?(link)
-        check_link_and_save(link)
-        @site_info[:links_found] += 1
+      if !link.nil?
+        if internal?(link)
+          check_link_and_save(link)
+        end
       end
       i+=1
     end
     # update array [1] to true
+  end
+
+  def clean_link(link)
+    link = link.split('?')[0]
+    link = link.split('#')[0]
   end
 
   def process_assets(document)
@@ -77,7 +95,7 @@ class Crawl
 
   def internal?(link)
     prefix = %w( http https // )
-    if !prefix.include?(URI.parse(link).scheme)
+    if !prefix.include?(URI.parse(link).scheme) && !link.start_with?("//")
       true
     else
       link = Addressable::URI.parse(link).host.downcase
@@ -86,15 +104,26 @@ class Crawl
         true
       else
         @site_info[:external_links_found] += 1
+        p "eternal link: " + link
         return false
       end
     end
   end
 
   def check_link_and_save(link)
-    uri = Addressable::URI.parse(link)
-    link = uri.path
-    @processed << [link,false,false,[]] unless @processed.any? {|x| x[0] == (link)}
+    link = clean_link(link)
+    if link =~ URI::regexp
+      uri = Addressable::URI.parse(link)
+      link = uri.path
+    end
+    unless link == nil || link == ''
+      unless @processed.any? {|x| x[0] == (link)}
+        @processed << [link,false,false,[]]
+        @site_info[:links_found] += 1
+      end
+    end
+    p @processed
+    p @site_info
   end
 
 end
