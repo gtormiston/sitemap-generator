3c27d35bfa55b062e69d4de7c642d49249b924e2
Graham Ormiston <grahamormiston@gmail.com>
2016-11-03 16:02:12 +0000

    implement basic asset scanning

--- lib/models/crawl.rb
+++ lib/models/crawl.rb
@@ -1,8 +1,10 @@
 # understands the content of a web page
 require "httparty"
+require 'net/http'
 require "uri"
 require "nokogiri"
 require "addressable/uri"
+require 'sanitize'
 require_relative "../helpers/http_helpers"
 
 class Crawl
@@ -12,48 +14,40 @@ class Crawl
   def initialize(url)
     @url = prepend_http(url)
     @processed = [["/",false,false,[]]] # [link,link_crawl_complete,asset_crawl_complete,'list of assets for link'
+    @external_links = []
     @site_info = { links_found: 1, links_crawled: 0, external_links_found: 0 }
   end
 
   def crawl
     url = Addressable::URI.parse(@url)
-    p url
     loop_through_links(url)
     # print page or send to page
   end
 
   def loop_through_links(url)
     while @site_info[:links_found] > @site_info[:links_crawled] do
-      #  loops through available, uncrawled urls in the @processed array
-      #  something like if @processed.any? x[0] == (link) && [1] == false
       @processed.each do |link_array|
         if link_array[1] == false
           p "crawling: " + link_array[0]
           body = get_body(url)
           process_links(body)
           link_array[1] = true
-          @site_info[:links_crawled] += 1
-        # elsif link_array[2] == false
-        #   process_assets(body)
-        #   link_array[2] = true
+          increment_crawled
+          assets = process_assets(body)
+          link_array[3] = assets
+          link_array[2] = true
         end
       end
-
-      p @site_info[:links_found]
-      p @site_info[:links_crawled]
-      #  then check if assets == true
-      # process_assets(body)
-      # else move on i++
     end
-    p "complete"
+    puts "***complete***"
   end
 
   def process_links(body)
-    document = parse_html(body)
+    doc = parse_html(body)
     i = 0
-    count = document.css('a').count
+    count = doc.css('a').count
     while i < count do
-      link = document.css('a')[i]['href']
+      link = doc.css('a')[i]['href']
       if !link.nil?
         if internal?(link)
           check_link_and_save(link)
@@ -61,36 +55,39 @@ class Crawl
       end
       i+=1
     end
-    # update array [1] to true
   end
 
-  def clean_link(link)
-    link = link.split('?')[0]
-    link = link.split('#')[0]
-  end
-
-  def process_assets(document)
-    # goes through the HTML and finds the image tags on the site
-    # replace this with all assets
-    i = 0
-    num = doc.xpath("//img").count
-    while i < num  do
-       puts doc.xpath("//img")[i]['src']
-       # check asset is local before saving?
-       i +=1
+  def process_assets(body)
+    doc = parse_html(body)
+    assets = []
+    doc.xpath('//script/@src').each do |link|
+      unless link.value.nil?
+        assets << clean_link(link.value)
+      end
     end
-    # update array [2] to true
+    doc.xpath('//img/@src').each do |link|
+      assets << clean_link(link.value)
+    end
+    doc.xpath('//link[@rel="stylesheet"]/@href').each do |link|
+      assets << clean_link(link.value)
+    end
+    return assets
   end
 
   private
 
   def get_body(url)
-    response = HTTParty.get(url)
+    response = HTTParty.get(URI.parse(url))
     response.body
   end
 
   def parse_html(html)
     Nokogiri::HTML(html)
+  end
+
+  def clean_link(link)
+    link = link.split('?')[0]
+    link = link.split('#')[0]
   end
 
   def internal?(link)
@@ -103,8 +100,8 @@ class Crawl
       if link == url
         true
       else
-        @site_info[:external_links_found] += 1
-        p "eternal link: " + link
+        @external_links << link
+        increment_external_found
         return false
       end
     end
@@ -119,11 +116,21 @@ class Crawl
     unless link == nil || link == ''
       unless @processed.any? {|x| x[0] == (link)}
         @processed << [link,false,false,[]]
-        @site_info[:links_found] += 1
+        increment_found
       end
     end
-    p @processed
-    p @site_info
+  end
+
+  def increment_crawled
+    @site_info[:links_crawled] += 1
+  end
+
+  def increment_found
+    @site_info[:links_found] += 1
+  end
+
+  def increment_external_found
+    @site_info[:external_links_found] += 1
   end
 
 end
